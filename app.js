5d36daa7855112e1fb2010085493b4b8b6c2623e
Philip Heyde <heyde.philip@gmail.com>
2022-01-20 23:09:22 +0100

    today's work

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
 app.set('view engine', 'pug')
 
 router.use('/profile/:userId', swimTimes.getSwimmerTimes);
--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,14 @@ var router = express.Router();
 var swimmerData = require('../data/swimmers.json');
 
 router.get('/profile/:userId', function(req, res, next) {
-  res.render('profile', { title: 'Profil', swimmerData:swimmerData, events:res.events });
+  res.render('profile', { title: 'Profil', swimmerData:getStaticSwimmerData(swimmerData, req.params.userId), events:res.events });
 });
+
+function getStaticSwimmerData(data, userId) {
+  var results = data.filter(function (entry) { return entry.id == userId; });
+  console.log('results', results[0]);
+
+  return results[0];
+}
 
 module.exports = router;
